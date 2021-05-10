09553fc97c757da2fe212291753bb450dc7a5053
i-icc <ichi623110@gmail.com>
2021-05-10 06:00:44 +0100

    a

--- content/create.table.sql
+++ content/create.table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE door_record (
     id INT(11) AUTO_INCREMENT,
     date_at datetime DEFAULT CURRENT_TIMESTAMP,
-    is_open NOT NULL,
-    PRIMARY KEY (id)
+    is_open boolean NOT NULL,
+    PRIMARY KEY (id),
 );
